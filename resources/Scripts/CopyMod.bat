e1489a564bf2e8ae611bf3c2adbbdc61172f8623
Jay Barker <jay@lolkek.com>
2020-03-13 08:31:26 +1000

    Ideas txt and helper scripts
    - Added some helpful scripts to copy the RimScape mod to the running directory and run Rimworld in quick mode

---
+++ resources/Scripts/CopyMod.bat
@@ -0,0 +1,3 @@
+rmdir /s /q "E:\Program Installs\Steam\steamapps\common\RimWorld\Mods\Rimscape-Core"
+mkdir "E:\Program Installs\Steam\steamapps\common\RimWorld\Mods\Rimscape-Core"
+xcopy /s /e "..\..\Rimscape-Core" "E:\Program Installs\Steam\steamapps\common\RimWorld\Mods\Rimscape-Core"
